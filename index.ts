5f8adb952299e9be2a087d251b7997c17df0d467
Firoze <firozecool@gmail.com>
2023-04-30 13:18:35 +0400

    improved looks of UI

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
+// grab the elements
 const runningPaceInKm: HTMLInputElement = document.querySelector("#runningPaceInKm")
 const runningPaceInMi: HTMLInputElement = document.querySelector("#runningPaceInMi")
 const runningPaceSlider: HTMLInputElement = document.querySelector("#runningPaceSlider")
+
+// set initial values
 runningPaceInKm.value = (parseInt(runningPaceSlider.value) * 2).toString()
 runningPaceInMi.value = (parseInt(runningPaceSlider.value) / 2).toString()
+
+// if slider is moved, change the values
 runningPaceSlider.addEventListener("input", (event) => {
   runningPaceInKm.value = (parseInt((event.target as HTMLInputElement).value) * 2).toString()
   runningPaceInMi.value = (parseInt((event.target as HTMLInputElement).value) / 2).toString()
