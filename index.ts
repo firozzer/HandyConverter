db2d298e6a858090c749771820336abc5896a3fa
Firoze <firozecool@gmail.com>
2023-05-01 17:24:52 +0400

    decided to keep only 1 slider for min/kmORmi awa kph/mph

--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
+// magic numbers
+const MAX_SLIDER_VAL = 900
+const SLIDER_MAX_1_MIN_PER_KM = 840 // Use this if you want max slider to be 1 min/km. It's used in formula ahead
+const SLIDER_MAX_2_MIN_PER_KM = 780 // Use this if you want max slider to be 1 min/km. It's used in formula ahead
+
 // grab the elements
 const runningPaceInKm: HTMLInputElement = document.querySelector("#runningPaceInKm")
 const runningPaceInMi: HTMLInputElement = document.querySelector("#runningPaceInMi")
 const runningPaceSlider: HTMLInputElement = document.querySelector("#runningPaceSlider")
 
 // set initial values
-runningPaceInKm.value = (parseInt(runningPaceSlider.value) * 2).toString()
-runningPaceInMi.value = (parseInt(runningPaceSlider.value) / 2).toString()
+const valOfSlider = parseInt(runningPaceSlider.value)
+const secsOfSlider = valOfSlider - SLIDER_MAX_1_MIN_PER_KM - (2 * (valOfSlider - MAX_SLIDER_VAL))
+runningPaceInKm.value =  new Date(secsOfSlider * 1000).toISOString().slice(14,19).replace(':', '')
+runningPaceInMi.value =  new Date((secsOfSlider * 1.60934) * 1000).toISOString().slice(14,19).replace(':', '')
 
-// if slider is moved, change the values
+// if slider is slid, change the valuessd
 runningPaceSlider.addEventListener("input", (event) => {
-  runningPaceInKm.value = (parseInt((event.target as HTMLInputElement).value) * 2).toString()
-  runningPaceInMi.value = (parseInt((event.target as HTMLInputElement).value) / 2).toString()
+  const valOfSlider = parseInt((event.target as HTMLInputElement).value)
+  const newSecsOfSlider = valOfSlider - SLIDER_MAX_1_MIN_PER_KM - (2 * (valOfSlider - MAX_SLIDER_VAL))
+  runningPaceInKm.value = new Date(newSecsOfSlider * 1000).toISOString().slice(14,19).replace(':', '')
+  runningPaceInMi.value = new Date((newSecsOfSlider * 1.60934) * 1000).toISOString().slice(14,19).replace(':', '')
 })
