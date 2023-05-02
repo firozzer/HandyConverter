d49fff74b159bae4198e950dbc98c8df75fd0555
Firoze <firozecool@gmail.com>
2023-05-02 12:46:55 +0400

    working on editable inputs

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ theSlider.value = SLIDER_VAL_AT_START.toString();
 // set initial values
 const valOfSlider = parseInt(theSlider.value);
 const secsOfSlider = valOfSlider - SLIDER_MAX_1_MIN_PER_KM - (2 * (valOfSlider - MAX_SLIDER_VAL));
-minPerKm.value = new Date(secsOfSlider * 1000).toISOString().slice(14, 19);
-minPerMi.value = new Date((secsOfSlider * 1.60934) * 1000).toISOString().slice(14, 19);
+minPerKm.value = new Date(secsOfSlider * 1000).toISOString().slice(14, 19).replace(':', '');
+minPerMi.value = new Date((secsOfSlider * 1.60934) * 1000).toISOString().slice(14, 19).replace(':', '');
 const someFrkyNum = 3600 / secsOfSlider;
 kph.value = (Math.round((someFrkyNum + Number.EPSILON) * 100) / 100).toString();
 // const mphNum = parseInt(kph.value) * 0.621371192
@@ -36,3 +36,41 @@ theSlider.addEventListener("input", (event) => {
     const someMiVal = 3600 / secsForAMi;
     mph.value = (Math.round((someMiVal + Number.EPSILON) * 100) / 100).toString();
 });
+minPerKm.addEventListener('input', (event) => {
+    let minsOfNum = 0;
+    const theNumInputted = event.target.value;
+    const inputIsValid = validateMMSSInput(theNumInputted);
+    if (inputIsValid) {
+        if (theNumInputted.length > 2) {
+            minsOfNum = parseInt(theNumInputted.slice(0, -2));
+        }
+        const secsOfNum = parseInt(theNumInputted.slice(-2));
+        if (secsOfNum > 59) {
+            console.warn("Secs wrong mannnnnn");
+        }
+        // console.log(`Got ${minsOfNum}min ${secsOfNum}secs`)
+        const totalSecs = (minsOfNum * 60) + secsOfNum;
+        const totalSecsForMile = totalSecs * 0.621371;
+        // console.log(totalSecs, totalSecsForMile, 'total secs')
+        const minOfMile = Math.floor(totalSecsForMile / 60);
+        // console.log(minOfMile, 'min of mile')
+        const secsOfMile = totalSecsForMile % 60;
+        // console.log(secsOfMile, 'secs of mile')
+        const secsRounded = Math.round((secsOfMile + Number.EPSILON));
+        // console.log(secsRounded, 'secsrounded')
+        minPerMi.value = `${minOfMile}${secsRounded}`;
+    }
+    else {
+        console.warn("ENTER CORRECT NUMBRO");
+    }
+});
+function validateMMSSInput(theNumInputted) {
+    const regex = /^\d{1,}$/;
+    const isValid = regex.test(theNumInputted);
+    if (isValid) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
