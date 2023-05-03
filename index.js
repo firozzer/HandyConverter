a56ff812e7b450146f29d3b43c4be4e0ee7e8852
Firoze <firozecool@gmail.com>
2023-05-03 12:40:39 +0400

    i think all inputs conversions done succyly

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ minPerKm.value = new Date(secsOfSlider * 1000).toISOString().slice(14, 19).repla
 minPerMi.value = new Date((secsOfSlider * 1.60934) * 1000).toISOString().slice(14, 19).replace(':', '');
 const someFrkyNum = 3600 / secsOfSlider;
 kph.value = (Math.round((someFrkyNum + Number.EPSILON) * 100) / 100).toString();
-// const mphNum = parseInt(kph.value) * 0.621371192
 const secsForAMi = secsOfSlider * 1.60934;
 const someMiVal = 3600 / secsForAMi;
 mph.value = (Math.round((someMiVal + Number.EPSILON) * 100) / 100).toString();
@@ -149,6 +148,108 @@ minPerMi.addEventListener('input', (event) => {
         mph.value = '';
     }
 });
+kph.addEventListener('input', (event) => {
+    const theNumInputted = event.target.value;
+    const inputIsValid = validationForKphMphInput(theNumInputted);
+    if (inputIsValid) {
+        const secsPerKm = 3600 / parseFloat(theNumInputted);
+        const secsPerMi = secsPerKm * 1.60934;
+        // doing kph to minPerKm
+        let minOfKm = Math.floor(secsPerKm / 60);
+        // console.log(minOfKm, 'min of Km')
+        const secsOfKm = secsPerKm % 60;
+        console.log(secsOfKm, 'secs of km');
+        let secsRounded = Math.round((secsOfKm + Number.EPSILON));
+        // console.log(secsRounded, 'secsrounded')
+        if (secsRounded < 10) { // padding zero in front if secss is single digit
+            secsRounded = 0 + secsRounded.toString();
+        }
+        else if (secsRounded == 60) { // if this then increasing min by 1 & padding 00 at end
+            secsRounded = '00';
+            minOfKm += 1;
+        }
+        minPerKm.value = `${minOfKm}${secsRounded}`;
+        // doing kph to minPerMi
+        let minOfMile = Math.floor(secsPerMi / 60);
+        // console.log(minOfMile, 'min of mile')
+        const secsOfMile = secsPerMi % 60;
+        console.log(secsOfMile, 'secs of mile');
+        let secsRoundedForMi = Math.round((secsOfMile + Number.EPSILON));
+        // console.log(secsRoundedForMi, 'secsroundedForMi')
+        if (secsRoundedForMi < 10) { // padding zero in front if secss is single digit
+            secsRoundedForMi = 0 + secsRoundedForMi.toString();
+        }
+        else if (secsRoundedForMi == 60) { // if this then increasing min by 1 & padding 00 at end
+            secsRoundedForMi = '00';
+            minOfMile += 1;
+        }
+        minPerMi.value = `${minOfMile}${secsRoundedForMi}`;
+        // // hereon doing stuff only for mph
+        const funkyNum = parseFloat(theNumInputted) * 0.621371;
+        mph.value = (Math.round((funkyNum + Number.EPSILON) * 100) / 100).toString();
+        // God only knows how i built this formula. Reason why i didn't keep slider same val as tot min/km secs is because i want inverse handling of slider.
+        const newValOfSlider = MAX_SLIDER_VAL - (secsPerKm - MIN_SLIDER_VAL) * 2 + (secsPerKm - MIN_SLIDER_VAL);
+        theSlider.value = newValOfSlider.toString();
+        // console.log('succuly done all', newValOfSlider, totalSecs)
+    }
+    else {
+        console.warn('WRONG NUM in KPH BROOOOOOOOO');
+        minPerKm.value = '';
+        minPerMi.value = '';
+        mph.value = '';
+    }
+});
+mph.addEventListener('input', (event) => {
+    const theNumInputted = event.target.value;
+    const inputIsValid = validationForKphMphInput(theNumInputted);
+    if (inputIsValid) {
+        const secsPerMi = 3600 / parseFloat(theNumInputted);
+        const secsPerKm = secsPerMi * 0.621371;
+        // doing mph to minPerKm
+        let minOfKm = Math.floor(secsPerKm / 60);
+        // console.log(minOfKm, 'min of Km')
+        const secsOfKm = secsPerKm % 60;
+        console.log(secsOfKm, 'secs of km');
+        let secsRounded = Math.round((secsOfKm + Number.EPSILON));
+        // console.log(secsRounded, 'secsrounded')
+        if (secsRounded < 10) { // padding zero in front if secss is single digit
+            secsRounded = 0 + secsRounded.toString();
+        }
+        else if (secsRounded == 60) { // if this then increasing min by 1 & padding 00 at end
+            secsRounded = '00';
+            minOfKm += 1;
+        }
+        minPerKm.value = `${minOfKm}${secsRounded}`;
+        // doing mph to minPerMi
+        let minOfMile = Math.floor(secsPerMi / 60);
+        // console.log(minOfMile, 'min of mile')
+        const secsOfMile = secsPerMi % 60;
+        console.log(secsOfMile, 'secs of mile');
+        let secsRoundedForMi = Math.round((secsOfMile + Number.EPSILON));
+        // console.log(secsRoundedForMi, 'secsroundedForMi')
+        if (secsRoundedForMi < 10) { // padding zero in front if secss is single digit
+            secsRoundedForMi = 0 + secsRoundedForMi.toString();
+        }
+        else if (secsRoundedForMi == 60) { // if this then increasing min by 1 & padding 00 at end
+            secsRoundedForMi = '00';
+            minOfMile += 1;
+        }
+        minPerMi.value = `${minOfMile}${secsRoundedForMi}`;
+        // // hereon doing stuff only for mph to kph
+        const funkyNum = parseFloat(theNumInputted) * 1.60934;
+        kph.value = (Math.round((funkyNum + Number.EPSILON) * 100) / 100).toString();
+        // God only knows how i built this formula. Reason why i didn't keep slider same val as tot min/km secs is because i want inverse handling of slider.
+        const newValOfSlider = MAX_SLIDER_VAL - (secsPerKm - MIN_SLIDER_VAL) * 2 + (secsPerKm - MIN_SLIDER_VAL);
+        theSlider.value = newValOfSlider.toString();
+        // console.log('succuly done all', newValOfSlider, totalSecs)
+    }
+    else {
+        console.warn('WRONG NUM in KPH BROOOOOOOOO');
+        minPerKm.value = '';
+        minPerMi.value = '';
+        kph.value = '';
+    }
+});
 function validateMMSSInput(theNumInputted) {
     const regex = /^\d{1,}$/;
     const isValid = regex.test(theNumInputted);
@@ -158,4 +259,11 @@ function validateMMSSInput(theNumInputted) {
     else {
         return false;
     }
+}
+function validationForKphMphInput(theNumInputted) {
+    if (parseFloat(theNumInputted) <= 0) {
+        return false;
+    }
+    else
+        return true;
 }
