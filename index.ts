87319b21995260b3cb8475b183cfaf998a4bd8dd
Firoze <firozecool@gmail.com>
2023-05-04 15:16:27 +0400

    incorporated DRY in CSS

--- index.ts
+++ index.ts
@@ -64,7 +64,6 @@ theRunningInputs.forEach(function(elem) {
     const theVal = theInputElem.value
     theInputElem.value = theVal.replace(':', '')
     theInputElem.setAttribute('type', 'number')
-    theInputElem.select();
   })
   elem.addEventListener('blur', (event) =>{
     const theInputElem = event.target as HTMLInputElement
