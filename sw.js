39799c979d00f4d320b9115a631ed4709e6423b1
Firoze <firozecool@gmail.com>
2023-05-10 23:50:49 +0400

    still finetuning sw ntwrk first cache later

--- sw.js
+++ sw.js
@@ -1,25 +1,25 @@
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("static").then(cache => {
-      return cache.addAll(["./", "index.css", "index.js", "icons/icon-512x512.png", "icons/icon-384x384.png", "icons/icon-256x256.png", "icons/icon-192x192.png"])
+      console.log("Caching all resourcesssss")
+      return cache.addAll(["./", "index.css", "index.js", "icons/icon-512x512.png", "icons/icon-384x384.png", "icons/icon-256x256.png", "icons/icon-192x192.png", "manifest.json", "favicon.ico"])
     })
   )
 })
 
+// below employs reliance on network first / cache later approach. 
 self.addEventListener("fetch", e => {
   if (navigator.onLine) {
     // If browser is online, fetch from internet
+    console.log("fetching from server")
     e.respondWith(fetch(e.request));
   } else {
     // If browser is offline, try to fetch from cache
     e.respondWith(
       caches.match(e.request).then(response => {
         if (response) {
-          console.log('serving from cache');
+          console.log('serving from cachee');
           return response;
-        } else {
-          // If resource is not found in cache, return a fallback response
-          return new Response("You are offline. Please check your internet connection.");
         }
       })
     );
