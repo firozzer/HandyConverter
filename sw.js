1a78b2fc18657ba4ca93f773e5247ccd32f48d58
Firoze <firozecool@gmail.com>
2023-05-10 23:35:50 +0400

    trying network first sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,27 @@
 self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open("static").then(cache => {
-            return cache.addAll(["./", "index.css", "index.js", "icons/icon-512x512.png", "icons/icon-384x384.png", "icons/icon-256x256.png", "icons/icon-192x192.png"])
-        })
-    )
+  e.waitUntil(
+    caches.open("static").then(cache => {
+      return cache.addAll(["./", "index.css", "index.js", "icons/icon-512x512.png", "icons/icon-384x384.png", "icons/icon-256x256.png", "icons/icon-192x192.png"])
+    })
+  )
 })
 
 self.addEventListener("fetch", e => {
+  if (navigator.onLine) {
+    // If browser is online, fetch from internet
+    e.respondWith(fetch(e.request));
+  } else {
+    // If browser is offline, try to fetch from cache
     e.respondWith(
-        caches.match(e.request).then(response => {
-            console.log('serving from cache')
-            return response || fetch(e.request)
-        })
-    )
-})
+      caches.match(e.request).then(response => {
+        if (response) {
+          console.log('serving from cache');
+          return response;
+        } else {
+          // If resource is not found in cache, return a fallback response
+          return new Response("You are offline. Please check your internet connection.");
+        }
+      })
+    );
+  }
+});
